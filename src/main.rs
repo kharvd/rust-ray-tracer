a00b241327f4bad68488602c218bd19c32991b5f
Valery Kharitonov <kharvd@gmail.com>
2020-12-22 18:00:36 -0500

    render simple ppm image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+fn float_to_int(v: f64) -> i32 {
+    return (255.999 * v) as i32;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let image_width = 256;
+    let image_height = 256;
+    println!("P3\n{} {}\n255", image_width, image_height);
+    for j in (0..image_height).rev() {
+        for i in 0..image_width {
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b = 0.25;
+
+            let ir = float_to_int(r);
+            let ig = float_to_int(g);
+            let ib = float_to_int(b);
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
