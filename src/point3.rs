c060c2852cf1ea4cb7affab20203861956eeb340
Valery Kharitonov <kharvd@gmail.com>
2020-12-24 15:29:32 -0500

    remove commented out code

--- src/point3.rs
+++ src/point3.rs
@@ -13,19 +13,4 @@ impl Point3 {
 
 impl_op_ex_commutative!(+ |lhs: &Point3, rhs: &Vec3| -> Point3 { Point3(lhs.0 + rhs.0, lhs.1 + rhs.1, lhs.2 + rhs.2) });
 impl_op_ex!(- |lhs: &Point3, rhs: &Vec3| -> Point3 { Point3(lhs.0 - rhs.0, lhs.1 - rhs.1, lhs.2 - rhs.2) });
-// impl_op_ex!(+= |lhs: &mut Point3, rhs: &Point3| { lhs.0 += rhs.0; lhs.1 += rhs.1; lhs.2 += rhs.2 });
-
 impl_op_ex!(- |lhs: &Point3, rhs: &Point3| -> Vec3 { Vec3(lhs.0 - rhs.0, lhs.1 - rhs.1, lhs.2 - rhs.2) });
-
-// impl_op_ex!(- |lhs: &Point3, rhs: f64| -> Point3 { Point3(lhs.0 - rhs, lhs.1 - rhs, lhs.2 - rhs) });
-// impl_op_ex!(- |lhs: f64, rhs: &Point3| -> Point3 { Point3(lhs - rhs.0, lhs - rhs.1, lhs - rhs.2) });
-// impl_op_ex!(-= |lhs: &mut Point3, rhs: &Point3| { lhs.0 -= rhs.0; lhs.1 -= rhs.1; lhs.2 -= rhs.2 });
-
-// impl_op_ex!(- |lhs: &Point3| -> Point3 { Point3(-lhs.0, -lhs.1, -lhs.2) });
-
-// impl_op_ex!(* |lhs: &Point3, rhs: &Point3| -> Point3 { Point3(lhs.0 * rhs.0, lhs.1 * rhs.1, lhs.2 * rhs.2) });
-// impl_op_ex_commutative!(* |lhs: &Point3, rhs: f64| -> Point3 { Point3(lhs.0 * rhs, lhs.1 * rhs, lhs.2 * rhs) });
-// impl_op_ex!(*= |lhs: &mut Point3, rhs: f64| { lhs.0 *= rhs; lhs.1 *= rhs; lhs.2 *= rhs; });
-
-// impl_op_ex!(/ |lhs: &Point3, rhs: f64| -> Point3 { Point3(lhs.0 / rhs, lhs.1 / rhs, lhs.2 / rhs) });
-// impl_op_ex!(/= |lhs: &mut Point3, rhs: f64| { lhs.0 /= rhs; lhs.1 /= rhs; lhs.2 /= rhs; });
